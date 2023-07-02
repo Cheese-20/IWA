8b02fec7d93444a1716947107a94c5b3e8f468d4
Cheese-20 <ssgamane64@gmail.com>
2023-07-02 21:18:07 +0200

    Completed extra challenge 2

---
+++ Extra challenge/Challenge 2/scricpt.js
@@ -0,0 +1,16 @@
+const normalValue = "40";
+const Day3Discount = "20";
+const Day7Discount = "50";
+
+let numDays = "3";
+let total ="";
+
+if(numDays>=7){
+ total=numDays*normalValue - Day7Discount;
+} else if(numDays>=3 && numDays<7){
+ total = numDays *normalValue - Day3Discount ;
+} else {
+    total = numDays * normalValue ;
+}
+
+console.log(total);
